87d21a21e5c0105b48b411ba406167363c76a308
Shikhar Dutta <149324583+unknownunicorn01@users.noreply.github.com>
2025-10-02 10:18:23 +0530

    Update a0_sorting.java

--- 1 java introduction programing/14 java sorting/a0_sorting.java
+++ 1 java introduction programing/14 java sorting/a0_sorting.java
@@ -1,1 +1,8 @@
-
+import java.util.*;
+public class a0_sorting{
+  public static void main(Srging[] args){
+    int[] arr = {4,8,7,1,-2};
+    Arrays.sort(arr);
+    System.out.pritnln(Arrays.toString(arr));
+   }
+}
