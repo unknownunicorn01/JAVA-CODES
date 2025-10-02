4e48a3ae470e8e5dd4bb9c491c8343846c153c36
Shikhar Dutta <149324583+unknownunicorn01@users.noreply.github.com>
2025-10-02 10:28:19 +0530

    Create a0_sorting.java

---
+++ 14 java sorting/a0_sorting.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class a0_sorting{
+  public static void main(String[] args){
+    int[] arr = {4,8,7,1,-2};
+    Arrays.sort(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+}
