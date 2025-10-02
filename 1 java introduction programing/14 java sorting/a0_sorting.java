53d12e41f9477bdac11ca8417defec834cfa68fc
Shikhar Dutta <149324583+unknownunicorn01@users.noreply.github.com>
2025-10-02 10:24:45 +0530

    Delete 1 java introduction programing/14 java sorting directory

--- 1 java introduction programing/14 java sorting/a0_sorting.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class a0_sorting{
-  public static void main(Srging[] args){
-    int[] arr = {4,8,7,1,-2};
-    Arrays.sort(arr);
-    System.out.pritnln(Arrays.toString(arr));
-   }
-}
